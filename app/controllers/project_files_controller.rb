79da35be078be223de6a87ece42f15a5c43f2a3c
thinkerzz <sccsalman2@gmail.com>
2017-12-02 22:37:58 +0500

    Back end implementation part 2

--- app/controllers/project_files_controller.rb
+++ app/controllers/project_files_controller.rb
@@ -1,9 +1,32 @@
 class ProjectFilesController < ApplicationController
 
   def index
+    @files = ProjectFile.all.order("created_at DESC")
   end
 
   def new
+    @file = ProjectFile.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def project_file_params
+    params.require(:project_file).permit(:file, :priority, :position)
   end
 
 end
